1640c1ff1b29358fa4977e67af9b54d114543725
Yan Wang <wangyan6783@gmail.com>
2018-06-24 20:16:08 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,18 @@
 def nyc_pigeon_organizer(data)
   new_data = {}
+  data.each { |category, info|
+    info.each { ||
+      
+    }
+  }
+  
+  
   colors = data[:color]
   colors.each { |color, names|
     names.each { |name|
       if new_data[name]
+        
+      end
     }
   }
 end
