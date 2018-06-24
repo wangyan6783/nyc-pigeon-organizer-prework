e0d2fed64e6519b16733a17f5a6431409df4c178
Yan Wang <wangyan6783@gmail.com>
2018-06-24 21:01:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = {}
+  data.each { |category, info|
+    info.each { |attributes, names|
+      names.each { |name|
+        if new_data[name] == nil
+          new_data[name] = {}
+          new_data[name][category] = [attributes.to_s]
+        elsif new_data[name][category] == nil
+          new_data[name][category] = [attributes.to_s]
+        else
+          new_data[name][category] << attributes.to_s
+        end
+      }
+    }
+  }
+  new_data
 end
